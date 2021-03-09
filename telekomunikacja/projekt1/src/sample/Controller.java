26e4acceef58ddca39c4ee5b704f735b9839375d
karwowskiantoni <229908@edu.p.lodz.pl>
2021-03-09 13:39:27 +0100

    wstępny szablon do wspólnej pracy

--- telekomunikacja/projekt1/src/sample/Controller.java
+++ telekomunikacja/projekt1/src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextArea dataField = new TextArea();
+    @FXML
+   TextField keyField1 = new TextField();
+    @FXML
+    TextField pathField = new TextField();
+    @FXML
+    TextArea resultField = new TextArea();
+    @FXML
+    Button encryptButtonText = new Button();
+    @FXML
+    Button decryptButtonText = new Button();
+    @FXML
+    Button encryptButtonFile = new Button();
+    @FXML
+    Button decryptButtonFile = new Button();
+    @FXML
+    Button directoryButton = new Button();
+    @FXML
+    Button saveFileButton = new Button();
 }
