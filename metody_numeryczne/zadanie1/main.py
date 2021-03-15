e9c20c231f293a1ffd8976f6dc3c5e3f259bb3a8
karwowskiantoni <229908@edu.p.lodz.pl>
2021-03-15 21:17:50 +0100

    totalnie totalne xd

--- metody_numeryczne/zadanie1/main.py
+++ metody_numeryczne/zadanie1/main.py
@@ -1,10 +1,10 @@
-import matplotlib as pl
+import matplotlib.pyplot as plt
 from functions import *
 
 if __name__ == '__main__':
     # todo
     # 1) interfejs konsolowy do wyboru funkcji z listy
     # 2) rysowanie wykresu
-    points = points()
+    points = [[1, 2, 3], [2, 4, 6]]
     plt.plot(points[0], points[1])
     plt.show()
