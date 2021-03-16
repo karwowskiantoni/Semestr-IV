d8c2ae97b7a52fd2be053e91c25f35bbab8cde26
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-03-16 13:39:44 +0100

    unimodal?

--- metody_numeryczne/zadanie1/functions.py
+++ metody_numeryczne/zadanie1/functions.py
@@ -6,6 +6,30 @@ def horner_method(x, coefficients):
     return value
 
 
+# zwraca przedziały na których funkcja 'function' może posiadać minimum
+# i maksimum lokalne z przedziału <'begin', 'end'> z dokładnością 'step'
+def unimodal_division(function, begin_x, end_x, step):
+    current_point = begin_x + step
+
+    divisions_with_maximum = []
+    divisions_with_minimum = []
+
+    iterations = int((end_x-begin_x)/step) - 2
+    for i in range(iterations):
+
+        if function(current_point) < function(current_point - step) and function(current_point) < function(current_point + step):
+            divisions_with_minimum.append(current_point)
+            print("minimum")
+
+        if function(current_point) > function(current_point - step) and function(current_point) > function(current_point + step):
+            divisions_with_maximum.append(current_point)
+            print("maksimum")
+
+        current_point += step
+
+    return [divisions_with_minimum, divisions_with_maximum]
+
+
 def points(function, begin, end, number):
     X = [0]*number
     Y = [0]*number
@@ -23,7 +47,3 @@ def maximum_in_range(function, begin, end, accuracy):
     return 0
 
 
-# todo implementacja przykładowych funkcji do wyboru
-# 2*x^3 + 4*x^2 + 2
-def first_option(x):
-    return horner_method(x, [2, 4, 0, 2])
