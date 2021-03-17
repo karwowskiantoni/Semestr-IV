869606ca105b5623d5e07772871faf3e5ed2795f
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-03-17 16:18:46 +0100

    dychotomy done + menu

--- metody_numeryczne/zadanie1/functions.py
+++ metody_numeryczne/zadanie1/functions.py
@@ -1,48 +1,55 @@
 # zwraca wartość wielomianu w punkcie 'x' obliczoną schematem
 # Hornera na podstawie współczynników wielomianu 'coefficients'
-def horner_method(x, coefficients):
+def calculate_by_horner_method(x, coefficients):
     value = coefficients[0]
     for i in range(1, len(coefficients)):
         value = value * x + coefficients[i]
     return value
 
 
-# zwraca przedziały na których funkcja 'function' może posiadać minimum
-# i maksimum lokalne z przedziału <'begin', 'end'> z dokładnością 'step'
-def unimodal_division(function, begin_x, end_x, step): # 200 kroków
-    current_point = begin_x + step
+def calculate_middle_of_the_division(division):
+    return division.begin_x + abs(division.end_x - division.begin_x)/2
+
+
+# zwraca 'number_of_divisions' przedziałów na których funkcja 'function' może
+# posiadać minimum lub maksimum lokalne z przedziału 'division'
+def calculate_unimodal_divisions(function, division, number_of_divisions):
+    step = abs(division.end_x - division.begin_x) / number_of_divisions
+    current_x = division.begin_x + step
 
     divisions_with_maximum = []
     divisions_with_minimum = []
 
-    iterations = int((end_x-begin_x)/step) - 2
-    for i in range(iterations):
+    for i in range(number_of_divisions):
 
-        if function(current_point) < function(current_point - step)\
-                and function(current_point) < function(current_point + step):
-            divisions_with_minimum.append(current_point)
-            print("minimum")
+        value_of_current_x = function(current_x)
+        value_of_current_x_minus_step = function(current_x - step)
+        value_of_current_x_plus_step = function(current_x + step)
 
-        if function(current_point) > function(current_point - step)\
-                and function(current_point) > function(current_point + step):
-            divisions_with_maximum.append(current_point)
-            print("maksimum")
+        if value_of_current_x < value_of_current_x_minus_step\
+                and value_of_current_x < value_of_current_x_plus_step:
+            divisions_with_minimum.append(Division(current_x-step, current_x + step))
 
-        current_point += step
+        if function(current_x) > value_of_current_x_minus_step\
+                and value_of_current_x > value_of_current_x_plus_step:
+            divisions_with_maximum.append(Division(current_x-step, current_x + step))
+
+        current_x += step
 
     return [divisions_with_minimum, divisions_with_maximum]
 
 
-# zwraca 'number' punktów funkcji 'function' z przedziału <'begin'; 'end'>
-def points(function, begin, end, number):
-    X = [0]*number
-    Y = [0]*number
-    step = (end-begin)/(number-1)
-    current_point = begin
-    for i in range(number):
-        X[i] = current_point
-        Y[i] = function(current_point)
-        current_point += step
+# zwraca 'number' punktów funkcji 'function' z przedziału division
+def calculate_points(function, division, number_of_points):
+    X = [0] * number_of_points
+    Y = [0] * number_of_points
+
+    step = abs(division.end_x - division.begin_x) / (number_of_points - 1)
+    current_x = division.begin_x
+    for i in range(number_of_points):
+        X[i] = current_x
+        Y[i] = function(current_x)
+        current_x += step
     return [X, Y]
 
 
@@ -50,4 +57,14 @@ def maximum_in_range(function, begin, end, accuracy):
     # todo po co to komu
     return 0
 
+def maximum_in_range_by_golden_division_method(function, division, accuracy, iterations_number):
+    # todo
+    return 0
 
+class Division:
+    begin_x = 0
+    end_x = 0
+
+    def __init__(self, begin_x, end_x):
+        self.begin_x = begin_x
+        self.end_x = end_x
--- metody_numeryczne/zadanie1/main.py
+++ metody_numeryczne/zadanie1/main.py
@@ -6,7 +6,7 @@ from functions import *
 
 def first_option(x):
     # 2*x^3 + 4*x^2 + 2
-    return horner_method(x, [2, 4, 0, 2])
+    return calculate_by_horner_method(x, [2, 4, 0, 2])
 
 
 def second_option(x):
@@ -18,10 +18,27 @@ if __name__ == '__main__':
     # todo 1) interfejs konsolowy do wyboru funkcji z listy
     # todo 2) rysowanie wykresu
 
-    wybrana_funkcja = second_option
-    punkty = points(wybrana_funkcja, -100, 100, 10000)
-    ekstrema = unimodal_division(wybrana_funkcja, -100, 100, 0.001)
+    print("1) 2*x^3 + 4*x^2 + 2")
+    print("2) sin(x)^3 + x")
+    print("wprowadź numer funkcji, przy blednym numerze sie pozegnamy: ")
+    co_powiedzial_idiota = input()
+    wybrana_funkcja = 0
+    if co_powiedzial_idiota == '1':
+        wybrana_funkcja = first_option
+    elif co_powiedzial_idiota == '2':
+        wybrana_funkcja = second_option
+    else:
+        print("Do widzenia")
+        sys.exit()
+    przedzialik = Division(-5, 5)
+    punkty = calculate_points(wybrana_funkcja, przedzialik, 10000)
+    przedzialy_z_ekstremami = calculate_unimodal_divisions(wybrana_funkcja, przedzialik, 30)
+    przedzialy_z_minimami = przedzialy_z_ekstremami[0]
+    przedzialy_z_maksimami = przedzialy_z_ekstremami[1]
 
-    plt.scatter(ekstrema[1][0], second_option(ekstrema[1][0]))
+    for przedzial in przedzialy_z_maksimami:
+        x = maximum_in_range_by_dychotomy_method(wybrana_funkcja, przedzial, 0, 10)
+        plt.scatter(x, wybrana_funkcja(x))
+
     plt.plot(punkty[0], punkty[1])
     plt.show()
