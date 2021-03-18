4ea2e23a6324eabac0d086b89e0587a0224f1e3d
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-03-18 18:57:13 +0100

    wstepny zarys postaci i wzmocnien

--- podstawy_analizy_cyklu_zycia/src/main/java/Postac.java
+++ podstawy_analizy_cyklu_zycia/src/main/java/Postac.java
@@ -1,3 +1,73 @@
 public class Postac {
+    public int iloscPunktowZycia;
+    public int regeneracjaPunktowZycia;
+    public int wartoscAtaku;
+    public int szybkoscAtaku;
+    public int wartoscPancerza;
+    public int procentowaSzansaNaTrafienie;
+    public int procentowaSzansaNaUnik;
+    public String opisPostaci;
+    public Wzmocnienie[] przedmioty;
+    public Wzmocnienie[] cechy;
 
+    public Postac(int iloscPunktowZycia, int regeneracjaPunktowZycia, int wartoscAtaku,
+                  int szybkoscAtaku, int wartoscPancerza, int procentowaSzansaNaTrafienie,
+                  int procentowaSzansaNaUnik, String opisPostaci, Wzmocnienie[] przedmioty, Wzmocnienie[] cechy) {
+        this.iloscPunktowZycia = iloscPunktowZycia;
+        this.regeneracjaPunktowZycia = regeneracjaPunktowZycia;
+        this.wartoscAtaku = wartoscAtaku;
+        this.szybkoscAtaku = szybkoscAtaku;
+        this.wartoscPancerza = wartoscPancerza;
+        this.procentowaSzansaNaTrafienie = procentowaSzansaNaTrafienie;
+        this.procentowaSzansaNaUnik = procentowaSzansaNaUnik;
+        this.opisPostaci = opisPostaci;
+        this.przedmioty = przedmioty;
+        this.cechy = cechy;
+    }
+
+    public boolean walka(Postac wrog, Wzmocnienie[] przedmioty) {
+        int inicjatywaBohatera = szybkoscAtaku;
+        int inicjatywaWroga = wrog.szybkoscAtaku;
+        while (true) {
+            if (zadajCios(wrog) == 0) {
+                System.out.println("Poślizgnął się na skórce od banana");
+            }
+            if (iloscPunktowZycia <= 0) {
+                return false;
+            }
+            if (wrog.iloscPunktowZycia <= 0){
+                return true;
+            }
+            break;
+        }
+        return true;
+    }
+
+    public String wypiszInformacjeOPostaci() {
+        return "Grzegorz Brzęczyszczykiewicz";
+    }
+
+    public int zadajCios(Postac wrog) {
+        if (czyTrafil(wrog)) {
+            int obrazenia = this.wartoscAtaku - wrog.wartoscPancerza;
+            if (obrazenia > 0) {
+                return obrazenia;
+            }
+            return 0;
+        }
+        return 0;
+    }
+
+    public boolean czyTrafil(Postac wrog) {
+        int ostatecznaSzansaNaTrafienie = procentowaSzansaNaTrafienie - wrog.procentowaSzansaNaUnik;
+        return Math.random() < (double) ostatecznaSzansaNaTrafienie / 100;
+    }
+
+    public int liczenieInicjatywy(Postac wrog, int inicjatywaBohatera, int inicjatywaWroga){
+        int roznicaSzybkosci = inicjatywaBohatera - inicjatywaWroga;
+        if (roznicaSzybkosci > 0) {
+            inicjatywaBohatera -= inicjatywaWroga;
+        }
+        return 0;
+    }
 }
--- podstawy_analizy_cyklu_zycia/src/main/java/Wzmocnienie.java
+++ podstawy_analizy_cyklu_zycia/src/main/java/Wzmocnienie.java
@@ -1,2 +1,36 @@
 public class Wzmocnienie {
+    public String typWzmocnienia;
+    public int iloscPunktowZycia;
+    public int regeneracjaPunktowZycia;
+    public int wartoscAtaku;
+    public int szybkoscAtaku;
+    public int wartoscPancerza;
+    public int procentowaSzansaNaTrafienie;
+    public int procentowaSzansaNaUnik;
+
+    public Wzmocnienie(String typWzmocnienia, int iloscPunktowZycia, int regeneracjaPunktowZycia,
+                       int wartoscAtaku, int szybkoscAtaku, int wartoscPancerza,
+                       int procentowaSzansaNaTrafienie, int procentowaSzansaNaUnik) {
+        this.typWzmocnienia = typWzmocnienia;
+        this.iloscPunktowZycia = iloscPunktowZycia;
+        this.regeneracjaPunktowZycia = regeneracjaPunktowZycia;
+        this.wartoscAtaku = wartoscAtaku;
+        this.szybkoscAtaku = szybkoscAtaku;
+        this.wartoscPancerza = wartoscPancerza;
+        this.procentowaSzansaNaTrafienie = procentowaSzansaNaTrafienie;
+        this.procentowaSzansaNaUnik = procentowaSzansaNaUnik;
+    }
+
+
+
+    public void wzmocnij(Postac bohater){
+        bohater.iloscPunktowZycia += iloscPunktowZycia;
+        bohater.regeneracjaPunktowZycia += regeneracjaPunktowZycia;
+        bohater.wartoscAtaku += wartoscAtaku;
+        bohater.szybkoscAtaku += szybkoscAtaku;
+        bohater.wartoscAtaku += wartoscAtaku;
+        bohater.wartoscPancerza += wartoscPancerza;
+        bohater.procentowaSzansaNaTrafienie += procentowaSzansaNaTrafienie;
+        bohater.procentowaSzansaNaUnik += procentowaSzansaNaUnik;
+    }
 }
