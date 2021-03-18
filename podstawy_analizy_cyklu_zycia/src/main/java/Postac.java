b41c4b25cfefb9e38dd33e254edf7d4384c33868
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-03-18 22:29:30 +0100

    O Bogowie walka gotowa

--- podstawy_analizy_cyklu_zycia/src/main/java/Postac.java
+++ podstawy_analizy_cyklu_zycia/src/main/java/Postac.java
@@ -6,13 +6,13 @@ public class Postac {
     public int wartoscPancerza;
     public int procentowaSzansaNaTrafienie;
     public int procentowaSzansaNaUnik;
-    public String opisPostaci;
+    public String nazwaPostaci;
     public Wzmocnienie[] przedmioty;
     public Wzmocnienie[] cechy;
 
     public Postac(int iloscPunktowZycia, int regeneracjaPunktowZycia, int wartoscAtaku,
                   int szybkoscAtaku, int wartoscPancerza, int procentowaSzansaNaTrafienie,
-                  int procentowaSzansaNaUnik, String opisPostaci, Wzmocnienie[] cechy) {
+                  int procentowaSzansaNaUnik, String nazwaPostaci, Wzmocnienie[] cechy) {
         this.iloscPunktowZycia = iloscPunktowZycia;
         this.regeneracjaPunktowZycia = regeneracjaPunktowZycia;
         this.wartoscAtaku = wartoscAtaku;
@@ -20,53 +20,120 @@ public class Postac {
         this.wartoscPancerza = wartoscPancerza;
         this.procentowaSzansaNaTrafienie = procentowaSzansaNaTrafienie;
         this.procentowaSzansaNaUnik = procentowaSzansaNaUnik;
-        this.opisPostaci = opisPostaci;
+        this.nazwaPostaci = nazwaPostaci;
         this.cechy = cechy;
     }
 
-    public boolean walka(Postac wrog, Wzmocnienie[] przedmioty) {
+    public boolean walka(Postac wrog) {
         int inicjatywaBohatera = szybkoscAtaku;
         int inicjatywaWroga = wrog.szybkoscAtaku;
+        int licznikTur = 1;
         while (true) {
-            if (zadajCios(wrog) == 0) {
-                System.out.println("Poślizgnął się na skórce od banana");
+            System.out.println();
+            System.out.println("Zaczela sie tura nr " + licznikTur);
+            System.out.println();
+            int[] daneTury = liczenieInicjatywy(inicjatywaBohatera, inicjatywaWroga);
+            if (daneTury[0] == 1){
+
+                if (daneTury[1] != 0){
+                    inicjatywaBohatera -= daneTury[1] * inicjatywaWroga;
+                } else {
+                    inicjatywaBohatera -= inicjatywaWroga;
+                }
+                inicjatywaWroga = 0;
+
+                int wynik = wymianaCiosow(wrog, daneTury);
+
+                if (wynik == 1){
+                    return true;
+                } else if (wynik == 2){
+                    return false;
+                }
+
+            } else {
+                if (daneTury[1] != 0){
+                    inicjatywaWroga -= daneTury[1] * inicjatywaBohatera;
+                } else {
+                    inicjatywaWroga -= inicjatywaBohatera;
+                }
+                inicjatywaBohatera = 0;
+
+                int wynik = wrog.wymianaCiosow(this, daneTury);
+
+                if (wynik == 1){
+                    return false;
+                } else if (wynik == 2){
+                    return true;
+                }
             }
-            if (iloscPunktowZycia <= 0) {
-                return false;
-            }
-            if (wrog.iloscPunktowZycia <= 0){
-                return true;
-            }
-            break;
+
+            inicjatywaBohatera += szybkoscAtaku;
+            inicjatywaWroga += wrog.szybkoscAtaku;
+            licznikTur++;
         }
-        return true;
     }
 
-    public String wypiszInformacjeOPostaci() {
+    private String wypiszInformacjeOPostaci() {
         return "Grzegorz Brzęczyszczykiewicz";
     }
 
-    public int zadajCios(Postac wrog) {
+    private boolean czyTrafil(Postac wrog) {
+        int ostatecznaSzansaNaTrafienie = procentowaSzansaNaTrafienie - wrog.procentowaSzansaNaUnik;
+        return Math.random() < ostatecznaSzansaNaTrafienie/100.0;
+    }
+
+    private int wyprowadzCios(Postac wrog) {
         if (czyTrafil(wrog)) {
             int obrazenia = this.wartoscAtaku - wrog.wartoscPancerza;
             if (obrazenia > 0) {
                 return obrazenia;
             }
+            System.out.println(nazwaPostaci + " atakuje łyżką zamiast mieczem");
             return 0;
         }
+        System.out.println(nazwaPostaci + " poślizgnął się na skórce od banana");
         return 0;
     }
 
-    public boolean czyTrafil(Postac wrog) {
-        int ostatecznaSzansaNaTrafienie = procentowaSzansaNaTrafienie - wrog.procentowaSzansaNaUnik;
-        return Math.random() < (double) ostatecznaSzansaNaTrafienie / 100;
+    private void zadajObrazenia(Postac wrog){
+        int obrazeniaZadanePrzezBohatera = wyprowadzCios(wrog);
+        if (obrazeniaZadanePrzezBohatera == 0) {
+        } else {
+            wrog.iloscPunktowZycia -= obrazeniaZadanePrzezBohatera;
+            System.out.println(nazwaPostaci + " zadanł " + wrog.nazwaPostaci + " " + obrazeniaZadanePrzezBohatera + " punktów obrażeń!");
+        }
     }
 
-    public int liczenieInicjatywy(Postac wrog, int inicjatywaBohatera, int inicjatywaWroga){
-        int roznicaSzybkosci = inicjatywaBohatera - inicjatywaWroga;
-        if (roznicaSzybkosci > 0) {
-            inicjatywaBohatera -= inicjatywaWroga;
+    private int wymianaCiosow(Postac wrog, int[] daneTury){
+        for (int i = 0; i < daneTury[1]; i++) {
+            zadajObrazenia(wrog);
+            if (wrog.iloscPunktowZycia <= 0) {
+                System.out.println(wrog.nazwaPostaci + "odszedł do krainy wiecznych łowów");
+                return 1;
+            }
+            System.out.println("Życie " + wrog.nazwaPostaci + " wynosi " + wrog.iloscPunktowZycia);
+        }
+        wrog.zadajObrazenia(this);
+        System.out.println("Życie " + nazwaPostaci + " wynosi " + iloscPunktowZycia);
+        if (iloscPunktowZycia <= 0) {
+            return 2;
         }
         return 0;
     }
+
+    private int[] liczenieInicjatywy(int inicjatywaBohatera, int inicjatywaWroga){
+        int roznicaSzybkosci = inicjatywaBohatera - inicjatywaWroga;
+        int[] info = new int[] {0,0};
+        if (roznicaSzybkosci > 0) {
+            int iloscAtakow = inicjatywaBohatera/inicjatywaWroga;
+            info[0] = 1;
+            info[1] = iloscAtakow;
+        } else {
+            int iloscAtakow = inicjatywaWroga/inicjatywaBohatera;
+            info[0] = 2;
+            info[1] = iloscAtakow;
+        }
+        return info;
+    }
 }
+
