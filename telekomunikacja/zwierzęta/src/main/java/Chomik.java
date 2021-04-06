24db56f2bcaa1f6c347648c0ae1328bf3e139649
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-04-06 19:53:44 +0200

    zadanie 2 numery

--- telekomunikacja/zwierzęta/src/main/java/Chomik.java
+++
@@ -1,9 +0,0 @@
-public class Chomik extends Zwierze{
-    public void xd(){
-        if(super.getRandomNumber(0, 100) >= 20) {
-            System.out.println("xd Chomik");
-        } else {
-            System.out.println("xd coś nie wyszło");
-        }
-    }
-}
