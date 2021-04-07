7de483ee630b6256e921ed67324f4895d225854b
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-04-07 15:37:34 +0200

    zadanie 2 interface

--- metody_numeryczne/zadanie2/main.py
+++ metody_numeryczne/zadanie2/main.py
@@ -2,4 +2,28 @@ import functions
 import utils
 
 if __name__ == '__main__':
-    functions.create_identity_matrix(utils.parse_matrix("matrices/first_matrix"))
+    print("Wprowadź numer macierzy którą chcesz wprowadzić: ")
+    answer = input()
+    if answer == '1':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/first_matrix"))
+    elif answer == '2':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/second_matrix"))
+    elif answer == '3':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/third_matrix"))
+    elif answer == '4':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/fourth_matrix"))
+    elif answer == '5':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/fifth_matrix"))
+    elif answer == '6':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/sixth_matrix"))
+    elif answer == '7':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/seventh_matrix"))
+    elif answer == '8':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/eighth_matrix"))
+    elif answer == '9':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/ninth_matrix"))
+    elif answer == '10':
+        functions.create_identity_matrix(utils.parse_matrix("matrices/tenth_matrix"))
+    else:
+        print("nie ma takiego numeru")
+        quit(0)
