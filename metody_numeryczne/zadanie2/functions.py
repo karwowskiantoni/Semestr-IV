763473cf7c5a5a7f498931834d8c0f34c684514b
karwowskiantoni <229908@edu.p.lodz.pl>
2021-04-07 15:18:32 +0200

    niby gotowe, wszystko działa

--- metody_numeryczne/zadanie2/functions.py
+++ metody_numeryczne/zadanie2/functions.py
@@ -1,32 +1,37 @@
 import utils
 
 
-def create_identity_matrix(parameters_matrix):
-    current_matrix = parameters_matrix
-    # current_values = values
-    matrix_size = len(parameters_matrix)
-    number_of_eliminations = matrix_size
+def try_swap_rows(matrix, k):
+    for i in range(len(matrix) - k):
+        if matrix[k][k] == 0:
+            pom = matrix[k]
+            matrix[k] = matrix[k + i]
+            matrix[k + i] = pom
+    return matrix
 
+
+def interrupt_if_zero(value, message):
+    if round(value, 10) == 0:
+        print(message)
+        exit()
+
+
+def create_identity_matrix(matrix):
+    current_matrix = matrix
+    matrix_height = len(matrix)
+    matrix_width = len(matrix[0])   # = matrix_height + 1
+    number_of_eliminations = matrix_height
     for k in range(number_of_eliminations):
-        # todo spytać czy da się rozróżnbić układ sprzeczny i nieoznaczony
-        # todo oraz czy dzielenie przez zero jest jedynym takim warunkiem
-        utils.interrupt_if_zero(current_matrix[k][k], "układ sprzeczny lub nieoznaczony")
-        new_matrix = utils.create_matrix(matrix_size, matrix_size + 1)
-        # new_values = create_matrix(matrix_size, 1)
-        # todo spytać czy nie da się tego zrobić na macierzy niekwadratowej, nie oddzielając values
-        for i in range(matrix_size):
+        try_swap_rows(current_matrix, k)
+        interrupt_if_zero(current_matrix[k][k], "układ sprzeczny lub nieoznaczony")
+        new_matrix = utils.create_matrix(matrix_height, matrix_width)
+        for i in range(matrix_height):
             if i == k:
-                # new_values[i] = current_values[k] / current_matrix[k][k]
-                for j in range(matrix_size + 1):
+                for j in range(matrix_width):
                     new_matrix[k][j] = current_matrix[k][j] / current_matrix[k][k]
             else:
-                # new_values[i] = current_values[i] - (current_values[k] * current_matrix[i][k]) / current_matrix[k][k]
-                for j in range(matrix_size + 1):
+                for j in range(matrix_width):
                     new_matrix[i][j] = current_matrix[i][j] - (current_matrix[k][j] * current_matrix[i][k]) / current_matrix[k][k]
 
         current_matrix = new_matrix
-        # current_values = new_values
-
     utils.print_matrix(utils.round_matrix(current_matrix, 1))
-    # print(round_matrix(current_values, 1))
-
--- metody_numeryczne/zadanie2/utils.py
+++ metody_numeryczne/zadanie2/utils.py
@@ -6,10 +6,10 @@ def print_matrix(array):
 def parse_matrix(filename):
     file = open(filename, "r")
     matrix = []
-    row = [int(i) for i in file.readline().split()]
+    row = [float(i) for i in file.readline().split()]
     while row:
         matrix.append(row)
-        row = [int(i) for i in file.readline().split()]
+        row = [float(i) for i in file.readline().split()]
     return matrix
 
 
@@ -25,14 +25,7 @@ def create_matrix(number_of_rows, number_of_columns):
     return [[float(0)] * number_of_columns for i in range(number_of_rows)]
 
 
-def interrupt_if_zero(value, message):
-    if value == 0:
-        print(message)
-        exit()
-
-
 def round_matrix(matrix, digits):
-    # todo spytać czy zaokrąglanie tylko w ostatnim etapie
     if not isinstance(matrix, list):
         return round_na_sterydach(matrix, digits)
     if not isinstance(matrix[0], list):
