70644a7acd3232f9304b8efba099756d6793d160
karwowskiantoni <229908@edu.p.lodz.pl>
2021-04-07 14:00:32 +0200

    wstęp do zapisu z pliku

--- metody_numeryczne/zadanie2/functions.py
+++ metody_numeryczne/zadanie2/functions.py
@@ -3,6 +3,14 @@ def print_matrix(array):
         print(row)
 
 
+def round_na_sterydach(value, digits):
+    rounded_value = round(value, digits)
+    if rounded_value == -0:
+        return float(0)
+    else:
+        return rounded_value
+
+
 def create_matrix(number_of_rows, number_of_columns):
     return [[float(0)] * number_of_columns for i in range(number_of_rows)]
 
@@ -13,28 +21,42 @@ def interrupt_if_zero(value, message):
         exit()
 
 
-def create_identity_matrix(matrix, values):
-    current_matrix = matrix
+def round_matrix(matrix, digits):
+    # todo spytać czy zaokrąglanie tylko w ostatnim etapie
+    if not isinstance(matrix, list):
+        return round_na_sterydach(matrix, digits)
+    if not isinstance(matrix[0], list):
+        return [round_na_sterydach(matrix[i], digits) for i in range(len(matrix))]
+    else:
+        return [[round_na_sterydach(matrix[i][j], digits) for j in range(len(matrix[0]))] for i in range(len(matrix))]
+
+
+def create_identity_matrix(parameters_matrix, values):
+    current_matrix = parameters_matrix
     current_values = values
-    matrix_size = len(matrix)
+    matrix_size = len(parameters_matrix)
     number_of_eliminations = matrix_size
 
     for k in range(number_of_eliminations):
+        # todo spytać czy da się rozróżnbić układ sprzeczny i nieoznaczony
+        # todo oraz czy dzielenie przez zero jest jedynym takim warunkiem
         interrupt_if_zero(current_matrix[k][k], "układ sprzeczny lub nieoznaczony")
         new_matrix = create_matrix(matrix_size, matrix_size)
-        new_values = [float(0)]*matrix_size
+        new_values = create_matrix(matrix_size, 1)
+        # todo spytać czy nie da się tego zrobić na macierzy niekwadratowej, nie oddzielając values
         for i in range(matrix_size):
             if i == k:
-                new_values[i] = round(current_values[k] / current_matrix[k][k], 3)
+                new_values[i] = current_values[k] / current_matrix[k][k]
                 for j in range(matrix_size):
-                    new_matrix[k][j] = round(current_matrix[k][j] / current_matrix[k][k], 3)
+                    new_matrix[k][j] = current_matrix[k][j] / current_matrix[k][k]
             else:
-                new_values[i] = round(current_values[i] - (current_values[k] * current_matrix[i][k]) / current_matrix[k][k], 3)
+                new_values[i] = current_values[i] - (current_values[k] * current_matrix[i][k]) / current_matrix[k][k]
                 for j in range(matrix_size):
-                    new_matrix[i][j] = round(current_matrix[i][j] - (current_matrix[k][j] * current_matrix[i][k]) / current_matrix[k][k], 3)
+                    new_matrix[i][j] = current_matrix[i][j] - (current_matrix[k][j] * current_matrix[i][k]) / current_matrix[k][k]
 
         current_matrix = new_matrix
         current_values = new_values
-    print_matrix(current_matrix)
-    print(current_values)
+
+    print_matrix(round_matrix(current_matrix, 1))
+    print(round_matrix(current_values, 1))
 
--- metody_numeryczne/zadanie2/main.py
+++ metody_numeryczne/zadanie2/main.py
@@ -1,10 +1,16 @@
-import functions as f
+def parse_matrix(filename):
+    file = open(filename, "r")
+    row = file.readline().split()
+    print(row)
+
 
 if __name__ == '__main__':
-    x = [[3, 3, 1], [2, 5, 7], [1, 7, 1]]
-    y = [12, 33, 8]
+    # todo wczytywanie macierzy z pliku z pliku
+    # x = [[3, 3, 1], [2, 5, 7], [1, 2, 1]]
+    # y = [12, 33, 8]
     # x = [[3, 3, 1], [2, 5, 7], [-4, -10, -14]]
     # y = [1, 20, -20]
     # x = [[3, 2, 1, -1], [5, -1, 1, 2], [1, -1, 1, 2], [7, 8, 1, -7]]
     # y = [0, -4, 4, 6]
-    f.create_identity_matrix(x, y)
+    # f.create_identity_matrix(x, y)
+    parse_matrix("first_matrix")
