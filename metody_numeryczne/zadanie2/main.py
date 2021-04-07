9b80551d690b31de5240ed490b41adba08558a27
karwowskiantoni <229908@edu.p.lodz.pl>
2021-04-07 14:24:46 +0200

    zrób kurwa macierze

--- metody_numeryczne/zadanie2/main.py
+++ metody_numeryczne/zadanie2/main.py
@@ -2,4 +2,4 @@ import functions
 import utils
 
 if __name__ == '__main__':
-    functions.create_identity_matrix(utils.parse_matrix("matrices/first_matrix"))
+    functions.create_identity_matrix(utils.parse_matrix("matrices/third_matrix"))
