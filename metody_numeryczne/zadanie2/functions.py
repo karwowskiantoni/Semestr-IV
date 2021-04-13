9089c590f1a173026e7f00094df9b01df7fe98d8
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-04-13 20:11:55 +0200

    Zadanie 2 metody numeryczne

--- metody_numeryczne/zadanie2/functions.py
+++ metody_numeryczne/zadanie2/functions.py
@@ -1,7 +1,7 @@
 import utils
 
 
-def try_swap_rows(matrix, k):
+def try_to_swap_rows(matrix, k):
     for i in range(len(matrix) - k):
         if matrix[k][k] == 0:
             pom = matrix[k]
@@ -10,20 +10,22 @@ def try_swap_rows(matrix, k):
     return matrix
 
 
-def interrupt_if_zero(value, message):
-    if round(value, 10) == 0:
-        print(message)
-        exit()
-
-
-def create_identity_matrix(matrix):
+def gauss_jordan_solver(matrix):
+    is_undefined = False
     current_matrix = matrix
     matrix_height = len(matrix)
     matrix_width = len(matrix[0])   # = matrix_height + 1
     number_of_eliminations = matrix_height
     for k in range(number_of_eliminations):
-        try_swap_rows(current_matrix, k)
-        interrupt_if_zero(current_matrix[k][k], "układ sprzeczny lub nieoznaczony")
+        current_matrix = try_to_swap_rows(current_matrix, k)
+        if round(current_matrix[k][k], 10) == 0:
+            if current_matrix[k][matrix_width-1] != 0:
+                utils.print_matrix(utils.round_matrix(current_matrix, 1))
+                print("uklad jest sprzeczny")
+                return
+            else:
+                is_undefined = True
+                continue
         new_matrix = utils.create_matrix(matrix_height, matrix_width)
         for i in range(matrix_height):
             if i == k:
@@ -34,4 +36,10 @@ def create_identity_matrix(matrix):
                     new_matrix[i][j] = current_matrix[i][j] - (current_matrix[k][j] * current_matrix[i][k]) / current_matrix[k][k]
 
         current_matrix = new_matrix
-    utils.print_matrix(utils.round_matrix(current_matrix, 1))
+
+    if is_undefined:
+        utils.print_matrix(utils.round_matrix(current_matrix, 1))
+        print("układ jest nieoznaczony")
+    else:
+        utils.print_matrix(utils.round_matrix(current_matrix, 1))
+        print("układ posiada jedno rozwiązanie w postaci wektora rozwiązań (ostatnia kolumna)")
--- metody_numeryczne/zadanie2/main.py
+++ metody_numeryczne/zadanie2/main.py
@@ -5,25 +5,25 @@ if __name__ == '__main__':
     print("Wprowadź numer macierzy którą chcesz wprowadzić: ")
     answer = input()
     if answer == '1':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/first_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/first_matrix"))
     elif answer == '2':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/second_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/second_matrix"))
     elif answer == '3':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/third_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/third_matrix"))
     elif answer == '4':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/fourth_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/fourth_matrix"))
     elif answer == '5':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/fifth_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/fifth_matrix"))
     elif answer == '6':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/sixth_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/sixth_matrix"))
     elif answer == '7':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/seventh_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/seventh_matrix"))
     elif answer == '8':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/eighth_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/eighth_matrix"))
     elif answer == '9':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/ninth_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/ninth_matrix"))
     elif answer == '10':
-        functions.create_identity_matrix(utils.parse_matrix("matrices/tenth_matrix"))
+        functions.gauss_jordan_solver(utils.parse_matrix("matrices/tenth_matrix"))
     else:
         print("nie ma takiego numeru")
         quit(0)
