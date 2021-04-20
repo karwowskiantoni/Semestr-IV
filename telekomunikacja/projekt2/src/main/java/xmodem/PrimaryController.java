ae3f77f7cc1ea3da7972e9f88a63f3facb2baef5
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-04-20 14:20:15 +0200

    xmodem skonczony

--- telekomunikacja/projekt2/src/main/java/xmodem/PrimaryController.java
+++ telekomunikacja/projekt2/src/main/java/xmodem/PrimaryController.java
@@ -1,26 +1,31 @@
 package xmodem;
 
 import com.fazecast.jSerialComm.SerialPort;
-import com.google.common.io.ByteStreams;
 import com.google.common.primitives.Bytes;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import xmodem.model.BitArray;
 
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.zip.CRC32;
+
+//Twórcy: Antoni Karwowski 229908, Michał Gebel 229879
+//Główna klasa odpowiadająca za widok aplikacji, w której wyjątkowo z braku potrzeby rozdzielania logiki od widoku
+//(ten przedmiot nie sprawdza dobrych technik programistycznych)
+//umieszczono również wszystkie funkcje służące za przesył i odbiór informacji w xmodem
+//Aplikacja powstawała współbieżnie za pomocą pluginu "Code with me" do IDE Intelij firmy JetBrains
+//W związku z tym cały kod powstał przy udziale dwóch osób jednocześnie
+//Przełożyo się to na lepsze zrozumienie zagadnienia i wyeliminowało przypadek, gdzie druga osoba nie wie co dzieje
+//się w danej części programu, nad którym nie pracowała.
+//Korzystamy z dwóch głównych funkcji - receiveFile - wystawienie odbiornika, sendFile - wystawienie nadawcy
+//
+
 
 public class PrimaryController {
     @FXML
@@ -43,7 +48,10 @@ public class PrimaryController {
     final int ACK = 6;
     final int EOT = 4;
     final int CAN = 24;
+    final int C = 67;
 
+
+    //wysyłanie wiadomości (ustawienie programu na funkcję nadajnika)
     public void sendFile() throws IOException {
         //ustawienie portu komunikacyjnego (COM2) oraz strumieni transmisji danych
         SerialPort port = SerialPort.getCommPorts()[1];
@@ -57,77 +65,47 @@ public class PrimaryController {
             System.exit(0);
         }
 
+        boolean isCRC = crcON.isSelected();
+
         System.out.println("Rozpoczęto połączenie, port " + port.getSystemPortName() + " otwarty: " + port.isOpen());
-        System.out.println("Odbiorca wysłał sygnał: " + inputStream.read());
-        System.out.println("Nadawca rozpoczyna wysyłanie wiadomości..." + System.lineSeparator());
+        System.out.println("Nadawca rozpoczyna wysyłanie wiadomości...");
 
-        if(checksumON.isSelected()) {
-            for (int i = 0; i < bytes.length / 128 + 1; i++) {
-                byte[] block = new byte[128];
-                int beginBlockIndex = i * 128;
-                int endBlockIndex = i * 128 + 128;
+        //wydzielenie bloków danych z pliku, który chcemy przesłać
+        for (int i = 0; i < bytes.length / 128 + 1; i++) {
+            byte[] block = new byte[128];
+            int beginBlockIndex = i * 128;
+            int endBlockIndex = i * 128 + 128;
 
-                if (endBlockIndex < bytes.length) {
-                    System.arraycopy(bytes, beginBlockIndex, block, 0, 128);
-                } else {
-                    System.arraycopy(bytes, beginBlockIndex, block, 0, 128 - (endBlockIndex - bytes.length));
-                }
-                int firstResponse;
-                do {
-                    outputStream.write(new byte[]{SOH, (byte) (i + 1), (byte) (~(i + 1))}); //nagłówek
-                    outputStream.write(block);  // wysylanie bloku
-                    outputStream.write(calculateChecksum(block)); //obliczanie checksuma
-                    firstResponse = inputStream.read();
-                    System.out.println("Wysłano blok numer " + (i + 1));
-                    System.out.println("Odpowiedź obiorcy to: " + firstResponse + System.lineSeparator());
-                } while (firstResponse != ACK);
+            if (endBlockIndex < bytes.length) {
+                System.arraycopy(bytes, beginBlockIndex, block, 0, 128);
+            } else {
+                System.arraycopy(bytes, beginBlockIndex, block, 0, 128 - (endBlockIndex - bytes.length));
             }
-            System.out.println("Zakończono wysyłanie bloków");
-            int lastResponse;
             do {
-                outputStream.write(EOT);
-                lastResponse = inputStream.read();
-
-            } while (lastResponse != ACK);
-            System.out.println("Odbiorca wysłał sygnał: " + lastResponse);
+                //przesłanie nagłówka
+                outputStream.write(new byte[]{SOH, (byte) (i + 1), (byte) (~(i + 1))}); // wysylanie nagłówka
+                outputStream.write(block);  // wysylanie bloku
+                //suma kontrolna crc
+                if(isCRC) {
+                    byte[] crc = ByteBuffer.allocate(4).putInt(calculateCrc(block)).array(); //obliczanie crc
+                    outputStream.write(new byte[]{crc[2], crc[3]});
+                }
+                //suma kontrolna suma bitowa
+                else {
+                    outputStream.write(calculateChecksum(block)); //obliczanie checksuma
+                }
+                System.out.println("Wysłano blok numer " + (i + 1));
+            } while (inputStream.read() != ACK);
         }
-//        } else {
-//
-//            for (int i = 0; i < bytes.length / 128 + 1; i++) {
-//                byte[] block = new byte[128];
-//                int beginBlockIndex = i * 128;
-//                int endBlockIndex = i * 128 + 128;
-//
-//                if (endBlockIndex < bytes.length) {
-//                    System.arraycopy(bytes, beginBlockIndex, block, 0, 128);
-//                } else {
-//                    System.arraycopy(bytes, beginBlockIndex, block, 0, 128 - (endBlockIndex - bytes.length));
-//                }
-//                int firstResponse;
-//                do {
-//                    outputStream.write(new byte[]{SOH, (byte) (i + 1), (byte) (~(i + 1))}); //nagłówek
-//                    outputStream.write(block);  // wysylanie bloku
-//                    String hex = Integer.toHexString(CRC16(block));
-//                    BitArray crcBitArray = BitArray.hexStringToBitArray(hex);
-//                    outputStream.write(crcBitArray.getBytes()); //obliczanie checksuma
-//                    firstResponse = inputStream.read();
-//                    System.out.println("Wysłano blok numer " + (i + 1));
-//                    System.out.println("Odpowiedź obiorcy to: " + firstResponse + System.lineSeparator());
-//                } while (firstResponse != ACK);
-//            }
-//            System.out.println("Zakończono wysyłanie bloków");
-//            int lastResponse;
-//            do {
-//                outputStream.write(EOT); // EOT
-//                lastResponse = inputStream.read();
-//
-//            } while (lastResponse != ACK);
-//            System.out.println("Odbiorca wysłał sygnał: " + lastResponse);
-//        }
+        System.out.println("Zakończono wysyłanie bloków");
+        //przeslanie eot, który sygnalizuje koniec przesyłania danych
+        int lastResponse;
+        do {
+            outputStream.write(EOT);
+            lastResponse = inputStream.read();
 
-
+        } while (lastResponse != ACK);
         System.out.println("Zamykanie połączenia...");
-        System.out.println("świzdu gwizdu com2 poszedl w pizdu");
         outputStream.close();
         inputStream.close();
         port.closePort();
@@ -141,13 +119,22 @@ public class PrimaryController {
         OutputStream outputStream = port.getOutputStream();
         InputStream inputStream = port.getInputStream();
         List<Byte> receivedData = new ArrayList<>();
-        outputStream.write(NAK);
+
+        if(!crcON.isSelected() & !checksumON.isSelected()){
+            System.out.println("Nie wybrano sposobu kodowania pliku, żegnam");
+            System.exit(0);
+        }
+
+        boolean isCRC = crcON.isSelected();
+        if(isCRC) {
+            outputStream.write(C);
+        }else {
+            outputStream.write(NAK);
+        }
         while (true) {
             try {
                 int header = inputStream.read();
-                if (header == SOH) {
-                    //przechodzimy do następnego bloku
-                } else if (header == CAN) {
+                if (header == CAN) {
                     System.out.println("sygnał CAN, zamknięto połączenie");
                     port.closePort();
                     inputStream.close();
@@ -159,29 +146,57 @@ public class PrimaryController {
                 }
             } catch (Exception e) {
                 System.out.println("minęło 10 sekund od wysłania wiadomości, wysyłanie ponowne");
-                outputStream.write(NAK);
+                if(isCRC) {
+                    outputStream.write(C);
+                }else {
+                    outputStream.write(NAK);
+                }
                 continue;
             }
 
-            byte[] response = inputStream.readNBytes(131);
-            byte[] block = new byte[128];
-            byte checksum = response[130];
-            System.arraycopy(response, 2, block, 0, 128);
-            if (calculateChecksum(block) != checksum) {
-                System.out.println("Nie zgadza się suma kontrolna");
-                outputStream.write(NAK);
-            } else {
-                receivedData.addAll(Bytes.asList(block));
-                System.out.println("odczytano blok numer " + response[0]);
-                outputStream.write(ACK);
-            }
+            byte[] response;
+            if(isCRC)
+                response = inputStream.readNBytes(132);
+            else
+                response = inputStream.readNBytes(131);
+            System.out.println("zakończono wysyłanie bloku");
 
+            byte[] block = new byte[128];
+            System.arraycopy(response, 2, block, 0, 128);
+
+            if(isCRC) {
+                int crc = ((response[130] & 0xff) << 8) | (response[131] & 0xff);
+                System.out.println(crc);
+                System.out.println(calculateCrc(block));
+                if (calculateCrc(block) != crc) {
+                    System.out.println("Nie zgadza się suma kontrolna");
+                    outputStream.write(NAK);
+                } else {
+                    receivedData.addAll(Bytes.asList(block));
+                    System.out.println("odczytano blok numer " + response[0]);
+                    outputStream.write(ACK);
+                }
+            }
+            else {
+                byte checksum = response[130];
+                if (calculateChecksum(block) != checksum) {
+                    System.out.println("Nie zgadza się suma kontrolna");
+                    outputStream.write(NAK);
+                } else {
+                    receivedData.addAll(Bytes.asList(block));
+                    System.out.println("odczytano blok numer " + response[0]);
+                    outputStream.write(ACK);
+                }
+            }
         }
+        System.out.println("Zakończono wysyłanie bloków");
+
         bytes = new byte[receivedData.size()];
         for(int i = 0; i < receivedData.size(); i++){
             bytes[i] = receivedData.get(i);
         }
         saveFile();
+        System.out.println("Zamykanie połączenia...");
         port.closePort();
         inputStream.close();
         outputStream.close();
@@ -234,21 +249,19 @@ public class PrimaryController {
         return sum;
     }
 
-    int calculate_crc(byte[] bytes) {
-        int i;
-        int crc_value = 0;
-        for (byte aByte : bytes) {
-            for (i = 0x80; i != 0; i >>= 1) {
-                if ((crc_value & 0x8000) != 0) {
-                    crc_value = (crc_value << 1) ^ 0x8005;
-                } else {
-                    crc_value = crc_value << 1;
-                }
-                if ((aByte & i) != 0) {
-                    crc_value ^= 0x8005;
-                }
+    //implementacja algorytmu wyliczania zaawansowanej sumy kontrolnej (cykliczny kod nadmiarowy(cyclic redundancy code))
+    private int calculateCrc(byte[] b) {
+        int crc, i;
+        crc = 0;
+        for (byte value : b) {
+            crc = crc ^ (int) value << 8;
+            for (i = 0; i < 8; i++) {
+                if ((crc & 0x8000) != 0)
+                    crc = crc << 1 ^ 0x1021;
+                else
+                    crc = crc << 1;
             }
         }
-        return crc_value;
+        return crc & 0xFFFF;
     }
 }
