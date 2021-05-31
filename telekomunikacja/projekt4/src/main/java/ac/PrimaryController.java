72fcb3f3cd3b844d340ae6410f6dad0f44381dcc
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-05-31 18:33:36 +0200

    z4 project done

--- telekomunikacja/projekt4/src/main/java/ac/PrimaryController.java
+++ telekomunikacja/projekt4/src/main/java/ac/PrimaryController.java
@@ -8,7 +8,15 @@ import java.io.*;
 import javax.sound.sampled.*;
 
 //Twórcy: Antoni Karwowski 229908, Michał Gebel 229879
-
+//Główna klasa odpowiadająca za widok i logikę aplikacji - wyjątkowo z braku potrzeby rozdzielania logiki od widoku
+//(ten przedmiot nie sprawdza dobrych technik programistycznych)
+//Aplikacja powstawała współbieżnie za pomocą pluginu "Code with me" do IDE Intelij firmy JetBrains
+//W związku z tym cały kod powstał przy udziale dwóch osób jednocześnie
+//Przełożyo się to na lepsze zrozumienie zagadnienia i wyeliminowało przypadek, gdzie druga osoba nie wie co dzieje
+//się w danej części programu, nad którym nie pracowała.
+//Program posiada dwie główne funkcjonalności
+//1.Nagrywanie dźwięku (input z karty dźwiękowej) i zapisanie go do pliku o rozszerzeniu .wav
+//2.Odtwarzanie nagranego dźwięku w programie.
 public class PrimaryController {
 
     @FXML
@@ -22,7 +30,7 @@ public class PrimaryController {
     public static void main(String[] args) {
         App.main(args);
     }
-
+    //funkcja odpowiadająca za odtwarzanie nagranego uprzednio dźwięku
     public void playSound() throws LineUnavailableException, UnsupportedAudioFileException, IOException, InterruptedException {
         Clip clip = AudioSystem.getClip();
         clip.open(AudioSystem.getAudioInputStream(new File("nagranie.wav")));
@@ -30,6 +38,7 @@ public class PrimaryController {
         Thread.sleep(clip.getMicrosecondLength() / 1000);
     }
 
+    //funkcja odpowiadająca za wyowołanie funkcji nagrywającej dźwięk. Tutaj glównie zarządzamy zachowaniem przycisków
     public void recordSound() throws LineUnavailableException{
         if(recordButton.getText().equals("Rozpocznij nagrywanie")) {
             captureAudio();
@@ -44,11 +53,12 @@ public class PrimaryController {
         }
     }
 
+    //zakończenie nagrywania
     private void stopAudio() {
         targetDataLine.stop();
         targetDataLine.close();
     }
-
+    //rozpoczęcie nagrywania dźwięku
     private void captureAudio() throws LineUnavailableException {
         audioFormat = new AudioFormat(8000.0F, 16, 1, true, false);
         DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
@@ -56,7 +66,7 @@ public class PrimaryController {
         CaptureThread captureThread = new CaptureThread();
         captureThread.start();
     }
-
+    //dodatkowy wątek odpowiedzialny za nagrywanie dźwięku w trakcie działania programu
     class CaptureThread extends Thread {
         @Override
         public void run() {
